00bc8bdbcafd2ae5849a046f84668e5446cfc3a4
BikoPython <stevennyandoya@gmail.com>
2021-07-15 00:03:13 +0300

    composition-api:provide-inject

---
+++ provide-inject/using-injectMyMarker.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script>
+import { inject } from 'vue'
+
+
+export default {
+   setup() {
+       const userLocation = inject('location', 'The Universe')
+       const userGeolocation = inject('geolocation')
+
+       return {
+           userLocation,
+           userGeolocation
+       }
+   } 
+}
+</script>
---
+++ provide-inject/using-provideMyMap.vue
@@ -0,0 +1,22 @@
+<template>
+    <MyMaker />
+</template>
+
+<script>
+import { provide } from 'vue'
+import MyMaker from './using-injectMyMarker.vue'
+
+export default {
+    components: {
+        MyMaker
+    },
+
+    setup() {
+        provide('location', 'North Pole')
+        provide('geolocation', {
+            longitude: 90,
+            latitude: 135
+        })
+    }
+}
+</script>
