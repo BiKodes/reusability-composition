0b6f9ba6a702ad7fc4921bfb6c58af52a1028e33
BikoPython <stevennyandoya@gmail.com>
2021-07-16 13:08:33 +0300

    Teleport

---
+++ Teleport/teleport-with-vue-components.vue
@@ -0,0 +1,30 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    const app = Vue.createApp({
+        template: `
+            <h1>Root instance</h1>
+            <parent-component />
+         `
+    })
+
+    app.component('parent-component', {
+        template: `
+           <h2>This is a parent component</h2>
+           <teleport to="#endofbody">
+                <child-component name="John"/>
+           </teleport> 
+         `
+    })
+
+    app.component('child-component', {
+        props: ['name'],
+        template: `
+            <div>Hello, {{ name }}</div>
+        `
+    })
+}
+</script>
---
+++ Teleport/teleport.vue
@@ -0,0 +1,40 @@
+<template>
+    <body>
+        <div style="position: relative">
+            <h3>Tooltips with Vue 3 Teleport</h3>
+            <div>
+                <modal-button></modal-button>
+            </div>
+        </div>
+    </body>
+</template>
+
+<script>
+
+export default {
+   app.component('modal-button', {
+       template:`
+            <button @click="modalOpen = true">
+                Open full screen modal! (Watch teleport)
+            </button>
+
+            <teleport to="body">
+                <div v-if="modalOpen" class="modal">
+                    <div>
+                        I'm a teleported modal! 
+                        (My parent is "body") 
+                        <button @click="modalOpen = false">
+                            Close
+                        <button>
+                    </div>
+                </div>
+            </teleport>
+            `
+            data() {
+                return {
+                    modalOpen: false
+                }
+            }
+   }) 
+}
+</script>
